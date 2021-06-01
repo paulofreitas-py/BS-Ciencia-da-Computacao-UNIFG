15d0f39ccb878119777c2dacdf1fe9b1ae3ffe76
paulofreitas-py <paulofreitas.py@protonmail.com>
2021-06-01 10:31:05 -0300

    Create Main.java

---
+++ cco3-Programacao-Orientada-Obejto/Aula 15/Collections/collections/src/ArrayList/Exemplo 1/Main.java
@@ -0,0 +1,14 @@
+package ArrayList;
+import java.util.ArrayList;
+
+public class Main{
+  public static void main(String[] args){
+    ArrayList<String> lista_de_carros = new ArrayList<String>();
+    lista_de_carros.add("Volvo");
+    lista_de_carros.add("BMW");
+    lista_de_carros.add("Ford");
+    lista_de_carros.add("Mazda");
+    lista_de_carros.add("Celta");
+    System.out.println(lista_de_carros);
+  }
+}
