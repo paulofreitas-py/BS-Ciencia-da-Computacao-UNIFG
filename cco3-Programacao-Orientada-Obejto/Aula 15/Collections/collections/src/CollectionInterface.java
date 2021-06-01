74e868db0e95c6e87917aada1fea6fb95cb91e98
paulofreitas-py <paulofreitas.py@protonmail.com>
2021-06-01 10:22:25 -0300

    Create CollectionInterface.java

---
+++ cco3-Programacao-Orientada-Obejto/Aula 15/Collections/collections/src/CollectionInterface.java
@@ -0,0 +1,17 @@
+//inacabado
+
+import java.io.*;
+import java.util.*;
+
+class Teste {
+    public static void main(String[] args)
+}
+
+    Vector<Integer> vetor = new Vector<Integer>();
+    for (int i = 1; i <= 5; i++){
+        vetor.add(i);
+    }
+    
+    System.out.println(vetor;)
+    vetor.remove(3);
+    System.out.print(vetor.get(i) + " ");
