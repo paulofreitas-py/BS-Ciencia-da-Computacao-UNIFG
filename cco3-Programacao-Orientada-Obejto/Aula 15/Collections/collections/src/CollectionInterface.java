77b88e4341bd44ca5c9e30fe94d4e207035bd71c
paulofreitas-py <paulofreitas.py@protonmail.com>
2021-06-01 10:26:01 -0300

    Update CollectionInterface.java

--- cco3-Programacao-Orientada-Obejto/Aula 15/Collections/collections/src/CollectionInterface.java
+++ cco3-Programacao-Orientada-Obejto/Aula 15/Collections/collections/src/CollectionInterface.java
@@ -1,17 +1,20 @@
-//inacabado
-
 import java.io.*;
 import java.util.*;
 
 class Teste {
-    public static void main(String[] args)
-}
-
+  public static void main(String[] args){
     Vector<Integer> vetor = new Vector<Integer>();
-    for (int i = 1; i <= 5; i++){
-        vetor.add(i);
+    for(int i = 1; i <= 5; i ++){
+      vetor.add(i);
     }
-    
-    System.out.println(vetor;)
+    System.out.println(vetor);
+
     vetor.remove(3);
-    System.out.print(vetor.get(i) + " ");
+
+    System.out.println(vetor);
+
+    for(int i = 0; i < vetor.size(); i++){
+      System.out.println(vetor.get(i) + " ");
+    }
+  }
+}
