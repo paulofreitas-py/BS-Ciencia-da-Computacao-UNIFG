961783235413845de3f87c0a7835fa20c67918ee
Paulo Freitas <42820569+paulofreitas-py@users.noreply.github.com>
2022-05-03 10:52:16 -0300

    N1 BD

---
+++ cco5-Praticas-de-banco-de-dados/Avaliação-N1/ScriptsProvaPráticasBD.sql
@@ -0,0 +1,16 @@
+# 1ª Avaliação de Práticas de Banco de Dados - Prova Prática #
+
+# Data: 03/05/2022
+# Professor: Jonathan Bandeira
+# Equipe de Alunos(as): 
+
+
+# Questões da Prova. Colocar a respectiva resposta abaixo de cada comentário.
+
+-- Questão 1
+   
+-- Questão 2
+
+-- Questão 3
+
+-- Questão 4
